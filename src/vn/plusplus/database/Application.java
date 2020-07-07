5aa7105e26f69d0a275b4a8ef18987b172bce42e
nhiempc <nhiem10596@gmail.com>
2020-07-07 15:16:21 +0700

    update 7/7/2020

--- src/vn/plusplus/database/Application.java
+++ src/vn/plusplus/database/Application.java
@@ -5,9 +5,7 @@ import vn.plusplus.database.models.LaptopEntity;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -31,66 +29,83 @@ public class Application {
             System.out.println("Connection Failed! Check output console");
             return;
         }
-
+        System.out.println("Tìm kiếm:");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập lựa chọn:");
-        System.out.println("1. Tìm kiếm latop theo hãng");
-        System.out.println("2. Tìm kiếm latop theo giá");
-        System.out.println("3. Tìm kiếm latop theo hãng và ổ SSD");
+        System.out.println("1. Tìm kiếm latop theo khoảng giá");
+        System.out.println("2. Tìm kiếm latop theo hãng");
+        System.out.println("3. Tìm kiếm latop theo kích thước màn hình");
+        System.out.println("4. Tìm kiếm latop theo cấu hình RAM");
+        System.out.println("5. Tìm kiếm latop theo cấu hình CPU");
+        System.out.println("6. Tìm kiếm latop theo loại máy tính");
+        System.out.println("7. Tìm kiếm latop theo card màn hình");
+        System.out.println("8. Sắp xếp tăng giảm");
+        System.out.println("Nhập lựa chọn(mỗi lựa chọn cách nhau dấu cách): ");
         LaptopService laptopService = new LaptopService(connection);
-
-        int option = scanner.nextInt();
-        scanner.nextLine();
-        switch (option){
-            case 1:
-                System.out.println("Nhập vào hãng:");
-                String maker = scanner.nextLine();
-                List<LaptopEntity> laptopEntities = laptopService.findAllByMaker(maker);
-                if(laptopEntities == null || laptopEntities.isEmpty()){
-                    System.out.println("Không tìm thấy");
-                }else{
-                    for(LaptopEntity result: laptopEntities){
-                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
-                    }
-                }
-                break;
-            case 2:
-                System.out.println("Nhập vào khoảng giá(VND): ");
-                System.out.println("Giá min: ");
-                Float priceFrom = scanner.nextFloat();
-                System.out.println("Giá max: ");
-                Float priceTo = scanner.nextFloat();
-                if(priceFrom == null){
-                    priceFrom = 0.0f;
-                }else if(priceTo == null){
-                    priceTo = laptopService.findMaxPrice();
-                }else if(priceFrom == null && priceTo == null){
-                    priceFrom = 0.0f;
-                    priceTo = laptopService.findMaxPrice();
-                }
-                List<LaptopEntity> listFindByPrice = laptopService.findAllByPrice(priceFrom,priceTo);
-                if(listFindByPrice == null || listFindByPrice.isEmpty()){
-                    System.out.println("Không tìm thấy");
-                }else{
-                    for(LaptopEntity result: listFindByPrice){
-                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
-                    }
-                }
-                break;
-            case 3:
-                System.out.println("Nhập vào hãng:");
-                String maker1 = scanner.nextLine();
-                System.out.println("Nhập vào loại ổ cứng SSD(VD: 256GB):");
-                String ssd = scanner.nextLine();
-                List<LaptopEntity> listFindByMakerAndSSD = laptopService.findAllByMakerAndSSD(maker1,ssd);
-                if(listFindByMakerAndSSD == null || listFindByMakerAndSSD.isEmpty()){
-                    System.out.println("Không tìm thấy");
-                }else{
-                    for(LaptopEntity result: listFindByMakerAndSSD){
-                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
-                    }
-                }
-                break;
+        String strOption = scanner.nextLine();
+        String[] arrOption = strOption.split(" ");
+        List<Integer> lstOption = new ArrayList<>();
+        for(int i = 0; i< arrOption.length;i++){
+            lstOption.add(Integer.parseInt(arrOption[i]));
         }
+        Collections.sort(lstOption);
+        List<LaptopEntity> laptopEntities = laptopService.findLaptop(lstOption);
+        if(laptopEntities==null||laptopEntities.isEmpty()){
+            System.out.println("Không tìm thấy");
+        }else{
+            for(LaptopEntity result: laptopEntities){
+                System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
+            }
+        }
+//        switch (option){
+//            case 1:
+//                System.out.println("Nhập vào hãng:");
+//                String maker = scanner.nextLine();
+//                List<LaptopEntity> laptopEntities = laptopService.findAllByMaker(maker);
+//                if(laptopEntities == null || laptopEntities.isEmpty()){
+//                    System.out.println("Không tìm thấy");
+//                }else{
+//                    for(LaptopEntity result: laptopEntities){
+//                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
+//                    }
+//                }
+//                break;
+//            case 2:
+//                System.out.println("Nhập vào khoảng giá(VND): ");
+//                System.out.println("Giá min: ");
+//                Float priceFrom = scanner.nextFloat();
+//                System.out.println("Giá max: ");
+//                Float priceTo = scanner.nextFloat();
+//                if(priceFrom == null){
+//                    priceFrom = 0.0f;
+//                }else if(priceTo == null){
+//                    priceTo = laptopService.findMaxPrice();
+//                }else if(priceFrom == null && priceTo == null){
+//                    priceFrom = 0.0f;
+//                    priceTo = laptopService.findMaxPrice();
+//                }
+//                List<LaptopEntity> listFindByPrice = laptopService.findAllByPrice(priceFrom,priceTo);
+//                if(listFindByPrice == null || listFindByPrice.isEmpty()){
+//                    System.out.println("Không tìm thấy");
+//                }else{
+//                    for(LaptopEntity result: listFindByPrice){
+//                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
+//                    }
+//                }
+//                break;
+//            case 3:
+//                System.out.println("Nhập vào hãng:");
+//                String maker1 = scanner.nextLine();
+//                System.out.println("Nhập vào loại ổ cứng SSD(VD: 256GB):");
+//                String ssd = scanner.nextLine();
+//                List<LaptopEntity> listFindByMakerAndSSD = laptopService.findAllByMakerAndSSD(maker1,ssd);
+//                if(listFindByMakerAndSSD == null || listFindByMakerAndSSD.isEmpty()){
+//                    System.out.println("Không tìm thấy");
+//                }else{
+//                    for(LaptopEntity result: listFindByMakerAndSSD){
+//                        System.out.println("Tên sản phẩm: "+result.getName()+" | Giá sản phẩm: "+result.getPrice()+"");
+//                    }
+//                }
+//                break;
+//        }
     }
 }
--- src/vn/plusplus/database/services/LaptopService.java
+++ src/vn/plusplus/database/services/LaptopService.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LaptopService {
     private Connection con;
@@ -24,6 +25,91 @@ public class LaptopService {
             return rs.getFloat(1);
         }catch (Exception e){
             System.out.println("Error when find price max!");
+        }
+        return null;
+    }
+    public List<LaptopEntity> findLaptop(List<Integer> lstOptions){
+        Scanner scanner = new Scanner(System.in);
+        String sql ="SELECT * FROM laptop WHERE ";
+        for(int i = 0; i< lstOptions.size();i++){
+            if(lstOptions.get(i)==1){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Nhập vào giá min: ");
+                Float priceFrom = sc.nextFloat();
+                System.out.println("Nhập vào giá max: ");
+                Float priceTo = sc.nextFloat();
+                if(priceFrom == null){
+                    priceFrom = 0.0f;
+                }else if(priceTo == null){
+                    priceTo = findMaxPrice();
+                }else if(priceFrom == null && priceTo == null){
+                    priceFrom = 0.0f;
+                    priceTo = findMaxPrice();
+                }
+            sql+= "price BETWEEN '"+priceFrom+ "' AND '" +priceTo+"'";
+            }else if(lstOptions.get(i)==2){
+                System.out.println("Nhập vào hãng: ");
+                String maker = scanner.nextLine();
+                sql+= "maker LIKE '%" + maker + "%'";
+            }else if(lstOptions.get(i)==3){
+                System.out.println("Nhập vào kích thước màn hình: ");
+                String screenSize = scanner.nextLine();
+                sql+= "screen_size LIKE '%" + screenSize + "%'";
+            }else if(lstOptions.get(i)==4){
+                System.out.println("Nhập vào dung lượng RAM: ");
+                String ram = scanner.nextLine();
+                sql+= "ram LIKE '%" + ram + "%'";
+            }else if(lstOptions.get(i)==5){
+                System.out.println("Nhập vào cấu hình CPU: ");
+                String cpu = scanner.nextLine();
+                sql+= "cpu LIKE '%" + cpu + "%'";
+            }else if(lstOptions.get(i)==6){
+                System.out.println("Nhập vào loại máy tính: ");
+                String type = scanner.nextLine();
+                sql+= "type LIKE '%" + type + "%'";
+            }else if(lstOptions.get(i)==7){
+                System.out.println("Nhập vào card màn hình: ");
+                String card = scanner.nextLine();
+                sql+= "card LIKE '%" + card + "%'";
+            }else if(lstOptions.get(i)==8){
+                System.out.println("Nhập vào kiểu sắp xếp(DESC or ASC):");
+                String sort = scanner.nextLine();
+                sql+= " ORDER BY price "+sort;
+            }
+            if(lstOptions.size()==1||lstOptions.get(i)==8||lstOptions.size()==2&&lstOptions.contains(8)||lstOptions.size()==2&&!lstOptions.contains(8)&&i==1||lstOptions.size()>2&&i==lstOptions.size()-2&&lstOptions.contains(8)||i==lstOptions.size()-1&&lstOptions.size()>2&&!lstOptions.contains(8)){
+                sql+="";
+            }else{
+                sql+=" AND ";
+            }
+        }
+        try{
+            List<LaptopEntity> response = new ArrayList<>();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()){
+                LaptopEntity laptopEntity = new LaptopEntity(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getFloat(10),
+                        rs.getString(11),
+                        rs.getString(12),
+                        rs.getFloat(13),
+                        rs.getInt(14),
+                        rs.getTimestamp(15),
+                        rs.getTimestamp(16)
+                );
+                response.add(laptopEntity);
+            }
+            return response;
+        }catch (Exception e) {
+            System.out.println("Error when find laptop!" + e);
         }
         return null;
     }
