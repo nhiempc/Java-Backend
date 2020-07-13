ca1bdc63633294dd1661c9f28f4e2e2236a15ef6
nhiempc <nhiem10596@gmail.com>
2020-07-13 10:47:28 +0700

    Add properties for Statistic Entity

--- src/vn/plusplus/database/models/StatisticEntity.java
+++ src/vn/plusplus/database/models/StatisticEntity.java
@@ -1,4 +1,40 @@
 package vn.plusplus.database.models;
 
 public class StatisticEntity {
+    private String maker;
+    private int sold;
+    private float totalMoney;
+
+    public StatisticEntity() {
+    }
+
+    public StatisticEntity(String maker, int sold, float totalMoney) {
+        this.maker = maker;
+        this.sold = sold;
+        this.totalMoney = totalMoney;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
+    }
+
+    public float getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(float totalMoney) {
+        this.totalMoney = totalMoney;
+    }
 }
