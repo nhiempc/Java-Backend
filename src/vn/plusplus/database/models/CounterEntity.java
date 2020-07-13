e39bf9143013536abc4f3ad8122204bbf45843e4
nhiempc <nhiem10596@gmail.com>
2020-07-13 10:36:45 +0700

    Add properties for Counter

--- src/vn/plusplus/database/models/CounterEntity.java
+++ src/vn/plusplus/database/models/CounterEntity.java
@@ -1,4 +1,30 @@
 package vn.plusplus.database.models;
 
 public class CounterEntity {
+    private String maker;
+    private Integer quantity;
+
+    public CounterEntity() {
+    }
+
+    public CounterEntity(String maker, Integer quantity) {
+        this.maker = maker;
+        this.quantity = quantity;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
